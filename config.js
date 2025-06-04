6cc6bb5f3622f4fe018dc226e5cfde0373bf6104
JackyYuen <jackyyuen10@gmail.com>
2025-06-05 03:22:19 +0800

    GGGG

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ export default {
     maxVirus: 50,
     slowBase: 4.5,
     logChat: 0,
-    networkUpdateFactor: 3,
+    networkUpdateFactor: 15,
     maxHeartbeatInterval: 5000,
     foodUniformDisposition: true,
     newPlayerInitialPosition: "farthest",
